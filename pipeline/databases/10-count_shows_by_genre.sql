b02450e175b6c1300bc56fedac40b42c98520552
Salua-Sa <slnulx5@gmail.com>
2026-05-08 18:51:21 +0200

    Fix task 10

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -3,4 +3,5 @@
 SELECT(tv_genres.name) AS genre,
 COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres ON(tv_genres.id=tv_show_genres.genre_id)
-ORDER BY(tv_genres.name) DESC
+GROUP BY(tv_genres.name)
+ORDER BY(number_of_shows) DESC
