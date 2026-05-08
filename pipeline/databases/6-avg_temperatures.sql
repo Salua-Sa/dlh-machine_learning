3c2883e836407a84d115753d532a9119c4f6c234
Salua-Sa <slnulx5@gmail.com>
2026-05-08 17:16:29 +0200

    Fix task 6

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Displays the average temperature by city ordered by temperature descending.
 SELECT city, AVG(temperature) AS avg_temp FROM temperature
-GROUP BY city WHERE avg_temp DESC
+GROUP BY city ORDER BY avg_temp DESC
