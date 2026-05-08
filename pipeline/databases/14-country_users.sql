702f3085ac68af584a27e296029ebf6d8b7938c4
Salua-Sa <slnulx5@gmail.com>
2026-05-08 20:31:58 +0200

    Fix task 14

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -1,8 +1,8 @@
 -- Creates a table users
 CREATE TABLE IF NOT EXISTS users(
-    id int NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY(`id`)
 )
