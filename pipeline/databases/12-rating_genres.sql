98fa8d83aae46853b6b72a4459bef2642f38fbd4
Salua-Sa <slnulx5@gmail.com>
2026-05-08 19:50:58 +0200

    Fix task 12

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -2,6 +2,7 @@
 SELECT(tv_genres.name) AS name,
 SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres JOIN tv_show_genres
-ON(tv_genres.id=tv_show_genres.genres_id)
+ON tv_genres.id=tv_show_genres.genres_id
+JOIN tv_show_ratings ON (tv_show_genres.show_id=tv_show_ratings.show_id)
 GROUP BY name
 ORDER BY rating DESC
