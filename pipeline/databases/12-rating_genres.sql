00f9cb883dcf1133d16234fc75e81cf2a61131fb
Salua-Sa <slnulx5@gmail.com>
2026-05-08 19:32:37 +0200

    Add task 12

---
+++ pipeline/databases/12-rating_genres.sql
@@ -0,0 +1,7 @@
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT(tv_genres.name) AS name,
+SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres JOIN tv_show_ratings
+ON(tv_genres.id=tv_show_ratings.show_id)
+GROUP BY name
+ORDER BY rating DESC
