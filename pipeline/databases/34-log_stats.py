d7a0603f3a1014fa4128dd55f14154b299904abb
Salua-Sa <slnulx5@gmail.com>
2026-05-09 20:05:29 +0000

    Fix task 34

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
     total_logs = collection.count_documents({})
-    print("{}logs".format(total_logs))
+    print("{} logs".format(total_logs))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
