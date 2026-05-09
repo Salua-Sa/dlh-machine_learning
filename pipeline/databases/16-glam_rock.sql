1a9ef7e6667206e8687003bb5a4689e6cc55d13a
Salua-Sa <slnulx5@gmail.com>
2026-05-09 08:08:05 +0000

    Fix task 16

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,6 +1,7 @@
 -- Lists all bands with Glam rock as their main style,
--- ranked by their longevity
+-- ranked by their longevity.
 SELECT band_name,
-IF(split IS NULL, 2020 - formed, split - formed) AS lifespan
-FROM metal_bands WHERE style LIKE 'Glam rock%'
+IFNULL(split, 2020) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE 'Glam rock%'
 ORDER BY lifespan DESC;
