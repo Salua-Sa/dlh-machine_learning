0c9f2dfb95c9996430fa71c0731b6709794cb356
Salua-Sa <slnulx5@gmail.com>
2026-05-09 19:08:40 +0000

    Fix task 32

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -2,8 +2,7 @@
 """ Python function that changes all topics of a school document"""
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name"""
-    mongo_collection.update(
+    mongo_collection.update_many(
         {"name": name},
-        {"$set": {"topics": topics}},
-        { "multi": True}
+        {"$set": {"topics": topics}}
 )
