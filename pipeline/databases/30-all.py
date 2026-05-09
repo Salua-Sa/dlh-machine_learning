a948ad47ba33496dd11d4fd72b9b0833bccd0b33
Salua-Sa <slnulx5@gmail.com>
2026-05-09 18:44:37 +0000

    Fix task 30

--- pipeline/databases/30-all.py
+++ pipeline/databases/30-all.py
@@ -1,6 +1,8 @@
-// Lists all documents in a collection.
+#!/usr/bin/env python3
+"""Lists_all Python function"""
 def list_all(mongo_collection):
-  documents = list(mongo_collection.find())
-  if documents == []:
-    return []
-  return documents
+    """Lists all documents in a collection."""
+    documents = list(mongo_collection.find())
+    if documents == []:
+        return []
+    return documents
