53a5fb8123c1f48ee1852f7c78c0396eaa291093
Salua-Sa <slnulx5@gmail.com>
2026-05-09 11:15:24 +0000

    Fix task 20

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,15 +1,10 @@
 -- Creates a stored procedure ComputeAverageScoreForUser
 --that computes and store the average score for a student.
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser (IN users.id INT)
-BEGIN
+CREATE PROCEDURE ComputeAverageScoreForUser (IN users_id INT)
   DECLARE avg_score FLOAT;
   SELECT AVG(score) INTO avg_score
   FROM corrections
-  WHERE corrections.user_id = users.id;
+  WHERE corrections.user_id = users_id;
   UPDATE users
   SET average_score = avg_score
-  WHERE id = users.id ;
-END //
-DELIMITER ;
+  WHERE id = users_id ;
