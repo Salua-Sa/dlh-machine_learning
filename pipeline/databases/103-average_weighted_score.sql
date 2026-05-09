4a9e8c08e9a83e2125249c25056df109005fc2e6
Salua-Sa <slnulx5@gmail.com>
2026-05-09 20:53:51 +0000

    Fix task 38

--- pipeline/databases/103-average_weighted_score.sql
+++ pipeline/databases/103-average_weighted_score.sql
@@ -1,14 +1,17 @@
 -- Creates a stored procedure ComputeAverageWeightedScoreForUser
 -- that computes and store the average weighted score for a student.
-CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
 DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
 BEGIN
-    DECLARE avg_weighted_score
-    SELECT SUM(score * weight) / SUM(weight)
-    FROM corrections
-    WHERE corrections.user_id = p_user_id;
+    DECLARE avg_weighted_score FLOAT
+    SELECT SUM(c.score * p.weight) / SUM(p.weight)
+    INTO avg_weighted_score
+    FROM corrections c
+    JOIN projects p
+    ON c.project_id = p.id
+    WHERE c.user_id = p_user_id;
     UPDATE users
     SET  average_score = avg_weighted_score
     WHERE id = p_user_id;
 END //
-DEÃLIMITER //
+DELIMITER;
