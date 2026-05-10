9b0338c0a64d61fbde9e01c90ed6fb6fe70470b2
Salua-Sa <slnulx5@gmail.com>
2026-05-10 07:11:01 +0000

    Add task 40

---
+++ pipeline/databases/105-students.py
@@ -0,0 +1,13 @@
+"""Python function that returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by avg_score"""
+    return mongo_collection.aggregate([
+        {"$students": {
+            "name": 1,
+            "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
