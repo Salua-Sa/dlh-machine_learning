467424b2c4f010c0324bf04085482bb3e5a01a81
Salua-Sa <slnulx5@gmail.com>
2026-05-10 08:15:56 +0000

    Fix task 41

--- pipeline/databases/106-log_stats.py
+++ pipeline/databases/106-log_stats.py
@@ -4,7 +4,7 @@
 from pymongo import MongoClient
 
 
-if__name__ == "__main__":
+if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
     total_logs = collection.count_documents({})
