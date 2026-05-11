185099be36b48e5ef3df1c970d3c58f7356ddb56
Salua-Sa <slnulx5@gmail.com>
2026-05-11 19:14:53 +0000

    Add task 2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    """This function return the shape of a matrix"""
+    shape_list = []
+    while isinstance(matrix, list):
+        shape_list.append(len(matrix))
+        matrix = matrix[0]
+    return shape_list
