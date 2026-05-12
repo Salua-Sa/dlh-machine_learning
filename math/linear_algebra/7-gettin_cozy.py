0d1d029b3e50bb0a3c81d0875b575b89dac0e84a
Salua-Sa <slnulx5@gmail.com>
2026-05-12 20:33:27 +0200

    Fix task 7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -13,12 +13,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
                 new_matrix.append(row[:])
             for row in mat2:
                 new_matrix.append(row[:])
+        return new_matrix
     if axis == 1:
         if len(mat1) != len(mat2):
             return None
         else:
-            new_row = []
             for i in range(len(mat1)):
-                new_row.append(mat1[i] + mat2[i])
-            new_matrix.append(new_row)
-    return new_matrix
+                new_matrix.append(mat1[i] + mat2[i])
+        return new_matrix
