3cdf60032e4412ca9f7ba1bd3e03a10a31bd664f
Salua-Sa <slnulx5@gmail.com>
2026-05-14 16:06:38 +0200

    Fix task 17

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -4,8 +4,6 @@
 
 def cat_matrices(mat1, mat2, axis=0):
     """Returns the concatenate of two matrices along a specific axis"""
-    if len(mat1) != len(mat2):
-        return None
     new_matrix = []
     if axis == 0:
         for row in mat1:
@@ -13,6 +11,12 @@ def cat_matrices(mat1, mat2, axis=0):
         for row in mat2:
             new_matrix.append(row)
         return new_matrix
+    if len(mat1) != len(mat2):
+        return None
     else:
         for i in range(len(mat1)):
-            new_matrix.append(cat_matrices(mat1[i], mat2[i], axis - 1))
+            result = cat_matrices(mat1[i], mat2[i], axis - 1)
+        if result is None:
+            return None
+        new_matrix.append(result)
+        return new_matrix
