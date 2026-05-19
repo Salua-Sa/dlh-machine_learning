b9dda981b946a681b7def370b753c0c7567e1efa
Salua-Sa <slnulx5@gmail.com>
2026-05-19 18:47:19 +0200

    Delete wrong directories

--- pipeline/databases/13-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- Creates a table users
-CREATE TABLE IF NOT EXISTS users(
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255),
-    PRIMARY KEY(`id`)
-)
