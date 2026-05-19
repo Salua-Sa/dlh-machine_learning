4e5cd3a35f2f3380e3fc854b38f79828ee3c7a68
Salua-Sa <slnulx5@gmail.com>
2026-05-19 19:59:05 +0200

    restore linear algebra files

--- linear_algebra/10-ill_use_my_scale.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module that calculates the shape of an array"""
-
-
-def np_shape(matrix):
-    """Returns the shape of a numpy.ndarray"""
-    return matrix.shape
--- linear_algebra/11-the_western_exchange.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module that transposes a matrix"""
-
-
-def np_transpose(matrix):
-    """Returns the transposes of a numpy.ndarray"""
-    return matrix.T.copy()
