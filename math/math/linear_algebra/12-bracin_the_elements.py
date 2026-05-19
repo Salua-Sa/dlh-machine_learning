b345ab37dd66abdb61f8bb00715a32b53247db26
Salua-Sa <slnulx5@gmail.com>
2026-05-19 20:29:34 +0200

    Delete math/math directory

--- math/math/linear_algebra/12-bracin_the_elements.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module that perfoms element-wise operations"""
-
-
-def np_elementwise(mat1, mat2):
-    """Returns +, -, *, / element-wise"""
-    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
