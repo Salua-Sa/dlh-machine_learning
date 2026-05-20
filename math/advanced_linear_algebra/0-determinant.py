5bd0c62b250327d70be2d21b9cbbd79e63392462
Salua-Sa <slnulx5@gmail.com>
2026-05-20 10:51:40 +0200

    Add task 0

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
+"""Module calculates the determinant of a matrix"""
+
+
+import signal
 
 
 def determinant(matrix):
+    """Returns the determinant of a matrix"""
+    if matrix == []:
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+    result = 0
+    for col in range(len(matrix)):
+        submatrix = []
+        for row in matrix[1:]:
+            new_row = []
+            for j in range(len(row)):
+                if j != col:
+                    new_row.append(row[j])
+            submatrix.append(new_row)
+        sign = (-1) ** col
+        result += sign * matrix[0][col] * determinant(submatrix)
+    return result
