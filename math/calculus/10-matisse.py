783fac154b84a3dd0c5d72bc08229bc26cb52100
Salua-Sa <slnulx5@gmail.com>
2026-06-03 17:47:22 +0200

    Fix task 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,10 +4,12 @@
 
 def poly_derivative(poly):
     """Returns the derivative of a polynomia"""
-    if not isinstance(poly, list):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
     new_list = []
     for i in range(len(poly)):
         if i >= 1:
             new_list.append(poly[i] * i)
+    if len(poly) == 1:
+        return [0]
     return new_list
