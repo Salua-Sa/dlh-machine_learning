8aa68457f19a39165410e56eb334b74088b505d2
Salua-Sa <slnulx5@gmail.com>
2026-06-03 15:33:27 +0200

    Add task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module calculates the summation of i squared"""
+
+
+def summation_i_squared(n):
+    """Returns the summation of i^2"""
+    if not isinstance(n, int) or n <= 0:
+        return None
+    else:
+        return int(n * (n + 1) * (2 * n + 1) / 6)
