f18bb314c0fe1469568d7181d3beb9e86be15568
Salua-Sa <slnulx5@gmail.com>
2026-06-04 18:34:51 +0200

    Fix task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Module that plots y as a line graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """plot y as a line graph"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, "r-")
