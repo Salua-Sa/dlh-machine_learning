37f2f803e42384470a9fddb416cc16ebe7af0a98
Salua-Sa <slnulx5@gmail.com>
2026-06-04 20:41:13 +0200

    Add task 4

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that plots a histogram of student scores for a project"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plots a histogram of student scores for a project"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.hist(student_grades, bins=range(0, 101, 10), cedgecolor="black")
