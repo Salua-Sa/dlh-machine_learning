a7f8c91399f29cd33a2efd4be813a7806fcc107a
Salua-Sa <slnulx5@gmail.com>
2026-06-09 16:38:31 +0200

    Update Poisson PMF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -4,9 +4,9 @@
 
 class Poisson:
     """This class represents a poisson distribution"""
-    e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
+        """Inisialization method"""
         self.data = data
         self.lambtha = float(lambtha)
         if lambtha <= 0:
@@ -21,3 +21,18 @@ class Poisson:
                 for i in range(len(data)):
                     sum_data = sum_data + data[i]
             self.lambtha = sum_data/len(data)
+
+    def pmf(self, k):
+        """Returns the PMF for a given number of successes"""
+        e = 2.7182818285
+        k_factorial = 1
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        original_k = k
+        while k > 0:
+            k_factorial = k_factorial * (k)
+            k = k - 1
+        pmf = (e**-self.lambtha * self.lambtha**original_k) / k_factorial
+        return pmf
