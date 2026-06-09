4081009c1053194f3aafe7959a559ba3d6c3d31b
Salua-Sa <slnulx5@gmail.com>
2026-06-09 18:24:07 +0200

    Update Normal PDF

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -33,3 +33,10 @@ class Normal:
     def x_value(self, z):
         """Returns the x-value of a given z-score"""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Returns the value of the PDF for a given x-value"""
+        pi = 3.1415926536
+        e = 2.7182818285
+        z = self.z_score(x)
+        return (1 / (self.stddev * (2 * pi) ** 0.5)) * e ** (-0.5 * z ** 2)
