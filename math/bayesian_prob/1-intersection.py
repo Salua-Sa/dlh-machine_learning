cf2b9f1ffef9011528ca7585bd7cfc47d2d81480
Salua-Sa <slnulx5@gmail.com>
2026-06-15 18:56:04 +0200

    Add task 1

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Module that calculates the intersection """
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """Reaturn  the intersection of obtaining this data"""
+    """with the various hypothetical probabilities"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is " +
+                         "greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    for i in P:
+        if i < 0 or i > 1:
+            raise ValueError("All values in P must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+    factorial_x = 1
+    factorial_n = 1
+    factorial_n_minos_s = 1
+    n_minus_x = n - x
+    x_original = x
+    n_original = n
+    while x > 0:
+        factorial_x *= x
+        x -= 1
+    while n > 0:
+        factorial_n *= n
+        n -= 1
+    while n_minus_x > 0:
+        factorial_n_minos_s *= n_minus_x
+        n_minus_x -= 1
+    comb = factorial_n / (factorial_x * factorial_n_minos_s)
+    likelihood = (comb
+                  * (P ** x_original)
+                  * ((1 - P) ** (n_original - x_original)))
+    return likelihood * Pr
