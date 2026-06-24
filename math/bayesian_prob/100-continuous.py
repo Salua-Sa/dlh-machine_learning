205cd036377d5b64917beacc37252a1f8c883a35
Salua-Sa <slnulx5@gmail.com>
2026-06-24 09:34:43 +0200

    Add task 4

---
+++ math/bayesian_prob/100-continuous.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module that calculates the posterior probability"""
+from scipy import special
+
+
+def posterior(x, n, p1, p2):
+    """Reaturn the posterior probability for the various hypothetical"""
+    """probabilities of developing severe side effects falls within"""
+    """a specific range given the data"""
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is " +
+                         "greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(p1, float) or p1 < 0 or p1 > 1:
+        raise ValueError("p1 must be a float in the range [0, 1]")
+    if not isinstance(p2, float) or p2 < 0 or p2 > 1:
+        raise ValueError("p2 must be a float in the range [0, 1]")
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
+    posterior = special.betainc(x + 1, n - x + 1, p2)
+    - special.betainc(x + 1, n - x + 1, p1)
+    return posterior
