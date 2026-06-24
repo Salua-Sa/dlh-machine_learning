1b188fae59591812dda0d5625b0c1bd9ec641c18
Salua-Sa <slnulx5@gmail.com>
2026-06-24 15:23:44 +0200

    Add task 0

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This module calculates the mean and covariance of a data set"""
+import numpy as np
+
+
+def mean_cov(X):
+    """Returns the mean and covariance matrix of X.
+    X has shape (n, d):
+    - n = number of data points
+    - d = number of dimensions in each dta point
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n = X.shape[0]
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0)
+    centered = X - mean
+    cov = centered.T @ centered / (n - 1)
+    return mean, cov
