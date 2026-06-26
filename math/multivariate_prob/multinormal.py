101094bc95e1043137ee4b6f70e9fb2e13a014ad
Salua-Sa <slnulx5@gmail.com>
2026-06-26 16:31:43 +0200

    Add task 3

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -14,9 +14,37 @@ class MultiNormal:
         """
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        n = data.shape[1]
+        d, n = data.shape
         if n < 2:
             raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
         centered = data - self.mean
         self.cov = centered @ centered.T / (n - 1)
+
+
+    def pdf(self, x):
+        """Returns the value of the PDF at a data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if x.shape[0] != d or x.shape[1] != 1:
+            raise ValueError("x must have the shape ({d}, 1)")
+        diff = x - self.mean
+        inverse_cov = np.linalg.inv(self.cov)
+        determinant_cov = np.linalg.det(self.cov)
+        exponent = -0.5 * diff.T @ inverse_cov @ diff
+        denominator = np.sqrt(((2 * np.pi) ** d) * determinant_cov)
+        result_pdf = np.exp(exponent) / denominator
+
+        return result_pdf[0][0]
+
+if __name__ == '__main__':
+    import numpy as np
+    from multinormal import MultiNormal
+
+    np.random.seed(0)
+    data = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000).T
+    mn = MultiNormal(data)
+    x = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 1).T
+    print(x)
+    print(mn.pdf(x))
