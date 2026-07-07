d10a8b1e2228a161f0617f45915c3b0f39624f29
Salua-Sa <slnulx5@gmail.com>
2026-07-07 16:36:59 +0200

    Add task 3. Rename

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module modifies a pandas DataFrame"""
+import pandas as pd
+
+
+def rename(df):
+    """Return a modified pandas DataFrame with Datatime and Close columns"""
+    df = df.rename(columns={"Timestamp": "Datetime"})
+    df['Datetime'] = pd.to_datetime(df[Datetime], unit="s")
+    df = df[["Datetime", "Close"]]
+    return df
