d9bc4f271b269ba919c08ac1a420d7fde772d257
Salua-Sa <slnulx5@gmail.com>
2026-07-08 14:22:09 +0200

    Fix file 7-high.py

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -6,10 +6,3 @@ def high(df):
     """Returns a sorted DataFrame by the High price in descending order"""
     df = df.sort_values(by="High", ascending=False)
     return df
-
-from_file = __import__('2-from_file').from_file
-
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
-
-df = high(df)
-print(df.head())
