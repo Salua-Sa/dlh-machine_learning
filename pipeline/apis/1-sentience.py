dd23f32ef47149a9d236ad71c010814b09b4b36e
Salua-Sa <slnulx5@gmail.com>
2026-07-16 17:02:44 +0200

    Add task 1-sentience.py

---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+"""
+import requests
+
+
+def sentientPlanets():
+    """Returns the list of names of the home planets of all sentient species
+    """
+    planets = []
+    url = "https://swapi-api.hbtn.io/api/species/"
+
+    while url:
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        species_list = data["results"]
+
+        for species in species_list:
+            classification = species["classification"]
+            designation = species["designation"]
+
+            if "sentient" in classification.lower() or
+            "sentient" in designation.lower():
+                homeworld_url = species["homeworld"]
+
+                if homeworld_url:
+                    planet_response = requests.get(homeworld_url)
+                    planet_data = planet_response.json()
+                    planets.append(planet_data["name"])
+
+        url = data["next"]
+
+    return planets
